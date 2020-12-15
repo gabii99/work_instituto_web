500437d765aec4906361390a2fd77f57a41601ce
gabriel <lau.gavotti@gmail.com>
2020-12-15 19:40:27 -0300

    version-11

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 //selectors
 let header = document.querySelector('.header');
 let hamburguerMenu = document.querySelector('.hamburguer-menu');
+let navLink = document.querySelector('.nav-item');
 
 hamburguerMenu.addEventListener('click', function () {
     header.classList.toggle('menu-open');
 })
+// navLink.addEventListener('click', function() {
+
+// })
